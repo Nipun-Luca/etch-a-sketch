a164c2349836b3810b301efa4ecb33ef95edf0a1
Nipun-Luca <lmuhudugama@gmail.com>
2023-09-04 16:24:37 +0100

    Added darken and lighten buttons
    When a grid square is clicked, the color inside would darken or lighten according to the option button clicked

--- script.js
+++ script.js
@@ -64,22 +64,28 @@ function changeBoxColor(){
 
 //Button
 const buttons = document.querySelectorAll('.option');
-let isToggled = false;
 
-buttons.forEach(element => {
-    element.addEventListener('click', () => {
-        isToggled = !isToggled;
-        element.classList.toggle('toggled', isToggled);
-    });
-});
+// buttons.forEach(element => {
+//     //Initialize isToggled property for each button
+//     element.isToggled = false;
+
+//     element.addEventListener('click', function() {
+//         // Toggle the isToggled property for the clicked button
+//         this.isToggled = !this.isToggled;
+        
+//         // Toggle the 'toggled' class based on the isToggled property
+//         this.classList.toggle('toggled', this.isToggled);
+//     });
+// });
 
 
 /*Extra: random colors*/
-let randomColorsButton = document.getElementById("random");
+const randomColorsButton = document.getElementById("random");
 let randomEnabled = false;
 
 randomColorsButton.addEventListener("click", () => {
     randomEnabled = !randomEnabled;
+    randomColorsButton.classList.toggle('toggled', this.isToggled);
     if (randomEnabled) {
         applyRandomColors();
     } else {
@@ -108,7 +114,7 @@ function getRandomColor() {
 
 
 //Clear grid of all colours
-let clearButton = document.getElementById("clear");
+const clearButton = document.getElementById("clear");
 
 clearButton.addEventListener("click", resetColors);
 
@@ -116,5 +122,72 @@ function resetColors() {
     let boxesArray = Array.from(allBoxes);
     boxesArray.forEach((div) => {
         div.style.backgroundColor = ''; // Reset to default background color
+    });
+}
+
+
+//Darken and lighten buttons
+const darkenButton = document.getElementById("darken");
+const lightenButton = document.getElementById("lighten");
+let isDarkenToggled = false;
+let isLightenToggled = false;
+
+darkenButton.addEventListener("click", () => {
+    if (!isDarkenToggled) {
+        isDarkenToggled = true;
+        isLightenToggled = false;
+        lightenButton.classList.remove("toggled");
+        darkenSquare();
+        // Additional logic for darken button here
+    } else {
+        isDarkenToggled = false;
+        // Handle untoggle behavior if needed
+    }
+    darkenButton.classList.toggle("toggled", isDarkenToggled);
+});
+
+lightenButton.addEventListener("click", () => {
+    if (!isLightenToggled) {
+        isLightenToggled = true;
+        isDarkenToggled = false;
+        darkenButton.classList.remove("toggled");
+        lightenSquare();
+        // Additional logic for lighten button here
+    } else {
+        isLightenToggled = false;
+        // Handle untoggle behavior if needed
+    }
+    lightenButton.classList.toggle("toggled", isLightenToggled);
+});
+
+function darkenSquare() {
+    let boxesArray = Array.from(allBoxes);
+    boxesArray.forEach((div) => {
+        div.addEventListener("click", () => {
+            const currentColor = getComputedStyle(div).backgroundColor;
+            const rgbArray = currentColor.match(/\d+/g);
+            
+            // Calculate 10% of black to add to each color channel
+            const increaseBy = Math.floor(25.5); // 10% of 255
+            const newColor = `rgb(${rgbArray[0] - increaseBy}, ${rgbArray[1] - increaseBy}, ${rgbArray[2] - increaseBy})`;
+            
+            div.style.backgroundColor = newColor;
+        })
+    });
+}
+
+function lightenSquare() {
+    let boxesArray = Array.from(allBoxes);
+    boxesArray.forEach((div) => {
+        div.addEventListener("click", () => {
+            const currentColor = getComputedStyle(div).backgroundColor;
+            const rgbArray = currentColor.match(/\d+/g);
+            
+            // Calculate 10% of white to add to each color channel
+            const decreaseBy = Math.floor(25.5); // 10% of 255
+            const newColor = `rgb(${Math.min(rgbArray[0] + decreaseBy, 255)}, ${Math.min(rgbArray[1] + decreaseBy, 255)}, ${Math.min(rgbArray[2] + decreaseBy, 255)})`;
+            
+            div.style.backgroundColor = newColor;
+        })
     });
 }
