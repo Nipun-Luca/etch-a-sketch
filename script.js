f91eb901ed650539782af899592744e3beb9f6ba
Nipun-Luca <lmuhudugama@gmail.com>
2023-09-04 15:20:33 +0100

    Randomize and clear button
    Create a button to randomize the color change when hovering the grid boxes, and a clear button to reset all the grid boxes

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ let gridValue = slider.value;
 let gridContainer = document.getElementById("container");
 let allBoxes = document.getElementsByClassName("grid-box");
 
+//Current color
+let currentColor = "black"; //default
+
+
+//Startup
 createGrid();
 changeBoxColor();
+
+
 function createGrid() {
     for(let i = 0; i < gridValue*gridValue; i++) {
         let div = document.createElement("div");
@@ -21,17 +28,25 @@ function removeGrid() {
     let boxesArray = Array.from(allBoxes);
 
     //Loop through the elements and remove them
-    boxesArray.forEach(function(allBoxes) {
-        allBoxes.remove();
+    boxesArray.forEach(function(box) {
+        box.remove();
     });
 }
 
 
+//Slider
+let grid_size_display = document.getElementById("grid-size");
+grid_size_display.textContent = `${gridValue} x ${gridValue}`;
 slider.addEventListener("input", function() {
     gridValue = slider.value;
+    grid_size_display.textContent = `${gridValue} x ${gridValue}`;
     removeGrid()
     createGrid();
-    changeBoxColor();
+    if(randomEnabled) {
+        applyRandomColors();
+    } else {
+        changeBoxColor();
+    }
 })
 
 
@@ -41,7 +56,65 @@ function changeBoxColor(){
 
     boxesArray.forEach(function(box) {
         box.addEventListener("mouseover", function() {
-            box.classList.add("grid-box-hovered");
+            box.style.backgroundColor = currentColor;
         });
+    });
+}
+
+
+//Button
+const buttons = document.querySelectorAll('.option');
+let isToggled = false;
+
+buttons.forEach(element => {
+    element.addEventListener('click', () => {
+        isToggled = !isToggled;
+        element.classList.toggle('toggled', isToggled);
+    });
+});
+
+
+/*Extra: random colors*/
+let randomColorsButton = document.getElementById("random");
+let randomEnabled = false;
+
+randomColorsButton.addEventListener("click", () => {
+    randomEnabled = !randomEnabled;
+    if (randomEnabled) {
+        applyRandomColors();
+    } else {
+        randomEnabled = false;
+        changeBoxColor();
+    }
+});
+
+function applyRandomColors() {
+    let boxesArray = Array.from(allBoxes);
+    boxesArray.forEach((div) => {
+        // Generate a random color and apply it as the background color
+        const randomColor = getRandomColor();
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = randomColor;
+        });
+    });
+}
+
+function getRandomColor() {
+    // Generate a random color in hex format (#RRGGBB)
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+
+
+//Clear grid of all colours
+let clearButton = document.getElementById("clear");
+
+clearButton.addEventListener("click", resetColors);
+
+function resetColors() {
+    let boxesArray = Array.from(allBoxes);
+    boxesArray.forEach((div) => {
+        div.style.backgroundColor = ''; // Reset to default background color
     });
 }
